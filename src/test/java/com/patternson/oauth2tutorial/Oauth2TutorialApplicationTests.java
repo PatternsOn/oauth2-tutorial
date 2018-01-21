8be8cbb5ff2f9f8073bbbf223b50fb279aad3fa8
TobiasPettersson <pettersson.tobias84@gmail.com>
2018-01-21 21:24:06 +0100

    init commit

---
+++ src/test/java/com/patternson/oauth2tutorial/Oauth2TutorialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.patternson.oauth2tutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2TutorialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
